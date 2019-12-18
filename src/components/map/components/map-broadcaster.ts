a4acdb858aa58eff3c5e0755c78ab56460668ae5
Kyryl Stronko <kirstronko@gmail.com>
2019-12-18 10:22:39 +0200

    broadcaster

--- src/components/map/components/map-broadcaster.ts
+++ src/components/map/components/map-broadcaster.ts
@@ -3,20 +3,36 @@ import { withDumbMapCtx } from '../hocs/with-dumb-map-ctx';
 import { MapService } from '../services';
 import { MapEventsProps, MapHandlerName, MapEventHandler } from '..';
 
+export type MapBroadcasterProps = {
+    [key in MapHandlerName]: MapEventHandler | null;
+};
+
 const MapBroadcasterComponent = ({
     mapService, 
     ...props
-}: MapEventsProps & {
+}: MapBroadcasterProps & {
     mapService: MapService
 }) => {
     useEffect(() => {
+        const prevHandlers = mapService.handlers;
         const eventKeys = Object.keys(props) as MapHandlerName[];
+        const prevEventKeys = Object.keys(prevHandlers) as MapHandlerName[];
 
         eventKeys.forEach((eventName) => {
-            mapService.addHandler(
-                eventName as MapHandlerName,
-                props[eventName] as MapEventHandler,
-            );
+            const handler = props[eventName] as MapEventHandler | null;
+
+            if (handler) {
+                mapService.addHandler(
+                    eventName as MapHandlerName,
+                    props[eventName] as MapEventHandler,
+                );
+            } else {
+                const prevHandler = prevHandlers[eventName] as MapEventHandler;
+
+                if (!prevHandler) return;
+
+                mapService.removeHandler(eventName, prevHandler);
+            }
         });
 
         return () => {
@@ -26,10 +42,17 @@ const MapBroadcasterComponent = ({
                     props[eventName] as MapEventHandler,
                 );
             });
+
+            prevEventKeys.forEach((eventName) => {
+                mapService.addHandler(
+                    eventName as MapHandlerName,
+                    prevHandlers[eventName] as MapEventHandler,
+                );
+            });
         }
     }, []);
 
     return null;
 }
 
-export const MapBroadcaster = withDumbMapCtx<MapEventsProps>(MapBroadcasterComponent);
+export const MapBroadcaster = withDumbMapCtx<MapBroadcasterProps>(MapBroadcasterComponent);
