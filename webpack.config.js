48e484279c5b26340318dcf90773f60e64ec56f1
Kyryl Stronko <kirstronko@gmail.com>
2020-01-04 18:15:03 +0200

    minor

--- webpack.config.js
+++ webpack.config.js
@@ -1,48 +1,52 @@
 const path = require('path');
-const CircularDependencyPlugin = require('circular-dependency-plugin')
+const CircularDependencyPlugin = require('circular-dependency-plugin');
 
-module.exports = (env, argv) => ({
-  entry: './src/index.ts',
-  output: {      
-      path: path.join(__dirname, './.dist'),      
-      filename: 'maps.js',
-      library: '@micelord/maps',     
-      libraryTarget: 'umd',      
-      publicPath: '/.dist/',
-      umdNamedDefine: true,
-  },  
-  resolve: {
-    extensions: ['.ts', '.tsx', '.js', '.json']
-  },
-  externals: {  
-    'react': {          
-      commonjs: "react",          
-      commonjs2: "react",          
-      amd: "React",          
-      root: "React"      
-    },      
-    'react-dom': {          
-      commonjs: "react-dom",          
-      commonjs2: "react-dom",          
-      amd: "ReactDOM",          
-      root: "ReactDOM"      
+module.exports = (env, argv) => {  
+  const isDevelopment = argv.mode === 'development';
+
+  return {
+    entry: './src/index.ts',
+    output: {      
+        path: path.join(__dirname, './.dist'),      
+        filename: 'maps.js',
+        library: '@micelord/maps',     
+        libraryTarget: 'umd',      
+        publicPath: '/.dist/',
+        umdNamedDefine: true,
+    },  
+    resolve: {
+      extensions: ['.ts', '.tsx', '.js', '.json']
     },
-  },
-  module: {
-    rules: [
-      { 
-        test: /\.tsx?$/, 
-        loader: 'ts-loader'
-      }
-    ]
-  },
-  devtool: argv.mode === 'development' ? 'eval-source-map' : 'source-map',
-  plugins: [
-    new CircularDependencyPlugin({
-      exclude: /a\.js|node_modules/,
-      failOnError: true,
-      allowAsyncCycles: false,
-      cwd: process.cwd(),
-    })
-  ]
-})
+    externals: {  
+      'react': {          
+        commonjs: "react",          
+        commonjs2: "react",          
+        amd: "React",          
+        root: "React"      
+      },      
+      'react-dom': {          
+        commonjs: "react-dom",          
+        commonjs2: "react-dom",          
+        amd: "ReactDOM",          
+        root: "ReactDOM"      
+      },
+    },
+    module: {
+      rules: [
+        { 
+          test: /\.tsx?$/, 
+          loader: 'ts-loader'
+        }
+      ]
+    },
+    devtool: isDevelopment ? 'eval-source-map' : 'source-map',
+    plugins: isDevelopment ? [
+      new CircularDependencyPlugin({
+        exclude: /a\.js|node_modules/,
+        failOnError: true,
+        allowAsyncCycles: false,
+        cwd: process.cwd(),
+      })
+    ] : []
+  }
+}
